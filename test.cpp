4c6d93eefa776b5025ec78e70e9a1deec2c909c9
Iris <agboolaisholaidreez>
2022-12-11 08:58:24 +0100

    update

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    const int a = 1;
+    int b = 2;
+    int c = a;
+    b = c;
+    int file_size = 1000; // snake case
+    int FileSize = 1000;  // pascal case
+    ::cout << a;
+    return 0;
+}
