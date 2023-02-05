f2ad65077ad0e8d681c9e8d115d4a8796e5200e9
Iris <agboolaisholaidreez>
2023-02-05 09:54:00 +0100

    update

--- src/lession.cpp
+++ src/lession.cpp
@@ -3,6 +3,18 @@
 int main()
 {
 
-    std::cout << "hello world";
-    return 0;
+    // int file_size = 10;
+    // double sale = 100.09;
+    // std::cout << sale;
+    // return 0;
+
+    // assigment
+    int a = 2;
+    int b = 4;
+    int c = a;
+    a = b;
+    b = c;
+
+    std::cout << a;
+    std::cout << b;
 }
