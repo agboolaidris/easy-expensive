05f82a7397a3a1732f3adf12c18947e26707d076
Iris <agboolaisholaidreez>
2023-02-05 11:18:05 +0100

    update

---
+++ src/iiput.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string user_name;
+    cout << "what is your name: " << endl;
+    cin >> user_name;
+    cout << "welcome onboard " << user_name;
+    // return 0;
+}
---
+++ src/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+double user_name;
+    // cin >> user_name;
+    // cout >> "welcome onboard " << user_name;
+    // return 0;
