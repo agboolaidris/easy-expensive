b0f6c651d34d6e971a3e157e220bd8feb3add832
Iris <agboolaisholaidreez>
2023-02-05 09:26:37 +0100

    uppdate

---
+++ src/std.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const double file_size = 10.2;
+    cout << "file size: " << file_size << endl
+         << "file size: " << file_size;
+    return file_size;
+}
